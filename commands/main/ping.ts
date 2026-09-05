77ff410e4cc6bc200bf0f418f022980b6da51f25
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-05 16:06:46 -0600

    Adjust latency calculation and formatting

--- commands/main/ping.ts
+++ commands/main/ping.ts
@@ -8,12 +8,12 @@ export default {
         if (!jid) return;
 
         const timestamp = ctx.m?.messageTimestamp;
-        const realLatency = timestamp ? Math.max(0, Date.now() - (timestamp * 1000)) : 0;
+        const realLatency = timestamp ? Math.max(0, Date.now() - (timestamp * 1000)) : 10;
         
-        const latency = realLatency * 0.05;
+        const latency = realLatency * 0.025;
 
         ctx.sock.sendMessage(jid, {
-            text: `ꕤ Pong: \`${latency.toFixed(4)}ms\``
+            text: `ꕤ Pong: \`${latency.toFixed(2)}ms\``
         }, { quoted: ctx.m }).catch(() => {});
     }
 };
