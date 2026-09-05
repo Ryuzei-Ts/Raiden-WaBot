54b9b8889472093a903e5843ad073c93c11ff129
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-05 16:02:22 -0600

    Update config.ts

--- config.ts
+++ config.ts
@@ -13,7 +13,8 @@ const config: Config = {
     prefix: '.',
     owner: [
         '5214436165999',
-        '524436165999'
+        '905363893523',
+        '905364919591'
     ],
     banner: 'https://cdn.ryuzei.xyz/files/mz6r0pg6.jpeg',
     icon: 'https://cdn.ryuzei.xyz/files/x21npy.jpeg'
