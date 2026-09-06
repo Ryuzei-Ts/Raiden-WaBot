4c707021022df5818f519f50dbb8cd25136e6f9a
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 04:53:43 -0600

    Update open.ts

--- commands/admin/open.ts
+++ commands/admin/open.ts
@@ -6,27 +6,11 @@ export default {
     admin: true,
     botAdmin: true,
     run: async ({ chat, sock, args, msg }: any) => {
-        const metadata = await sock.groupMetadata(chat).catch(() => null);
-        
-        if (metadata && !metadata.announce) {
-            return msg.reply('✰ El grupo ya está *abierto*.');
-        }
-
-        const setGroupState = async (open: boolean) => {
-            const action = open ? 'not_announcement' : 'announcement';
-            if (typeof sock.groupSettingUpdate === 'function') {
-                return sock.groupSettingUpdate(chat, action);
-            }
-            if (typeof sock.groupSettingsUpdate === 'function') {
-                return sock.groupSettingsUpdate(chat, action);
-            }
-        };
-
         const timeStr = args?.[0];
         const match = timeStr?.match(/^(\d+)(s|m|h)$/i);
 
         if (!match) {
-            await setGroupState(true).catch(() => {});
+            await sock.groupSettingUpdate(chat, 'not_announcement').catch(() => {});
             return msg.reply('✰ El grupo ha sido *abierto*. Todos los participantes pueden enviar mensajes.');
         }
 
@@ -37,7 +21,7 @@ export default {
         msg.reply(`✰ El grupo se *abrirá* automáticamente en *${value}${unit}*.`);
 
         setTimeout(() => {
-            setGroupState(true).catch(() => {});
+            sock.groupSettingUpdate(chat, 'not_announcement').catch(() => {});
         }, delay);
     }
 };
