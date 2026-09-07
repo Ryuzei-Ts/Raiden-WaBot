12e51c4b9c55f432535763cce85bec216be4630d
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:47:20 -0600

    Add logopubgfem command for PUBG mascot logo

---
+++ commands/logomaker/logopubgfem.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logopubgfem',
+    description: 'Crea un logo mascota PUBG para equipos de esports',
+    effectUrl: 'https://en.ephoto360.com/pubg-mascot-logo-maker-for-an-esports-team-612.html'
+});
