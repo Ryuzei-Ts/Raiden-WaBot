996bd0a393514244b67de7bc3b5a2c141564d4e2
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:39:54 -0600

    Add logoglitch command for digital glitch logo effect

---
+++ commands/logomaker/logoglitch.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logoglitch',
+    description: 'Crea un logo con efecto de texto glitch digital',
+    effectUrl: 'https://en.ephoto360.com/create-digital-glitch-text-effects-online-767.html'
+});
