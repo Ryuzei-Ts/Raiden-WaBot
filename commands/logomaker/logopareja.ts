020c17245c179c660667c8494e537ee395c3eaaf
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:39:33 -0600

    Add logopareja command for logo creation

---
+++ commands/logomaker/logopareja.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logopareja',
+    description: 'Crea un logo con efecto de sombra y luz solar',
+    effectUrl: 'https://en.ephoto360.com/sunlight-shadow-text-204.html'
+});
