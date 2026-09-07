c0d92ccf4f76c8c955f50a47d6d39177221e5b46
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:46:46 -0600

    Add logoarmy command for military-style logo creation

---
+++ commands/logomaker/logoarmy.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logoarmy',
+    description: 'Crea un logo de estilo militar y FPS gaming',
+    effectUrl: 'https://en.ephoto360.com/free-gaming-logo-maker-for-fps-game-team-546.html'
+});
