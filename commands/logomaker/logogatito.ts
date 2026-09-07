bac3c1d9758bfbd457c0ae2d251d392079e7c01e
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:42:12 -0600

    Add logogatito command for foggy glass logo creation

---
+++ commands/logomaker/logogatito.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logogatito',
+    description: 'Crea un logo con texto escrito en vidrio empañado',
+    effectUrl: 'https://en.ephoto360.com/handwritten-text-on-foggy-glass-online-680.html'
+});
