b9404a1dd1784d4408447d6d393e53141ef978c4
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:54:48 -0600

    Create logodragonball command for logo generation
    Adds a new command for creating Dragon Ball style logos.

---
+++ commands/logomaker/logodragonball.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: ['logodragonball', 'logodb', 'dblogo'],
+    description: 'Crea un logo con efecto de texto estilo Dragon Ball',
+    effectUrl: 'https://en.ephoto360.com/create-dragon-ball-style-text-effects-online-809.html'
+});
