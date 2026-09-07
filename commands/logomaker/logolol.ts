391717b8fcff9188003afca82f8f5841ee6b3063
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:47:45 -0600

    Add logolol command for League of Legends logo

---
+++ commands/logomaker/logolol.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logolol',
+    description: 'Crea un logo estilo League of Legends',
+    effectUrl: 'https://en.ephoto360.com/make-your-own-league-of-legends-wallpaper-full-hd-442.html'
+});
