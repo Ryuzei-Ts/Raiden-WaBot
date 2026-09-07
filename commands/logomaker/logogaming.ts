3cec1e47de8aa1758315e8fe3fe5aa6e7b2524b3
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:40:52 -0600

    Add logogaming command for esports logo creation

---
+++ commands/logomaker/logogaming.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logogaming',
+    description: 'Crea un logo de equipo de esports y gaming',
+    effectUrl: 'https://en.ephoto360.com/make-team-logo-online-free-432.html'
+});
