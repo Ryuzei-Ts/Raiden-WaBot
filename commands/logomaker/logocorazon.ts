77e1932fc64201bbfcb703f085ffa6f37ceee7dc
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:54:26 -0600

    Create logocorazon.ts

---
+++ commands/logomaker/logocorazon.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: ['logocorazon', 'logoheart', 'corazonlogo'],
+    description: 'Crea un logo con efecto de corazón y linterna',
+    effectUrl: 'https://en.ephoto360.com/text-heart-flashlight-188.html'
+});
