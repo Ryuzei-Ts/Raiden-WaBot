d08b54541dc432b714efcaec32de1f39ece542a4
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:48:02 -0600

    Add logoamongus command for logo creation

---
+++ commands/logomaker/logoamongus.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logoamongus',
+    description: 'Crea un logo estilo Among Us',
+    effectUrl: 'https://en.ephoto360.com/create-a-cover-image-for-the-game-among-us-online-762.html'
+});
