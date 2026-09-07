016b6e468c1b23968d40a6ddf54d3cfd235ceb08
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:37:58 -0600

    Add logochristmas command for Christmas logo effect

---
+++ commands/logos/logochristmas.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logochristmas',
+    description: 'Crea un logo con efecto navideño',
+    effectUrl: 'https://en.ephoto360.com/christmas-effect-by-name-376.html'
+});
