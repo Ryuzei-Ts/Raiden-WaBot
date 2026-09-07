437d52377d28f8400eafde39640f4d61c8b6642a
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:43:40 -0600

    Add logonube command for cloud text effect logo

---
+++ commands/logomaker/logonube.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logonube',
+    description: 'Crea un logo con efecto de texto en las nubes',
+    effectUrl: 'https://en.ephoto360.com/cloud-text-effect-139.html'
+});
