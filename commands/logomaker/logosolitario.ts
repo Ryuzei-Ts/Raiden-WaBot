9578f411e72865543c5a27aedb18667e5d550806
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:41:14 -0600

    Create logosolitario command for typography logo
    Added a new command for creating a typography logo effect on pavement.

---
+++ commands/logomaker/logosolitario.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logosolitario',
+    description: 'Crea un logo con efecto tipográfico sobre pavimento',
+    effectUrl: 'https://en.ephoto360.com/create-typography-text-effect-on-pavement-online-774.html'
+});
