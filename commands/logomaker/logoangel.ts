7b537a1f13b0d5cd51afaa4b32c00ed54b68ab68
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:44:13 -0600

    Add logoangel command for angel wing logo effect

---
+++ commands/logomaker/logoangel.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logoangel',
+    description: 'Crea un logo con efecto de alas de ángel',
+    effectUrl: 'https://en.ephoto360.com/angel-wing-effect-329.html'
+});
