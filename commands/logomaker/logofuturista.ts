f26dd8cd6e51a5717ae9442b3903ff9bf5515b4d
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:43:15 -0600

    Add logofuturista command for futuristic logo creation

---
+++ commands/logomaker/logofuturista.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logofuturista',
+    description: 'Crea un logo con efecto de luz tecnológica futurista',
+    effectUrl: 'https://en.ephoto360.com/light-text-effect-futuristic-technology-style-648.html'
+});
