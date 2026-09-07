0496c0ceff69e1b4ea5dad744e4a448ec1375d86
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:46:05 -0600

    Add logohorror command for blood writing effect

---
+++ commands/logomaker/logohorror.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logohorror',
+    description: 'Crea un logo con efecto de escritura de sangre',
+    effectUrl: 'https://en.ephoto360.com/blood-writing-text-online-77.html'
+});
