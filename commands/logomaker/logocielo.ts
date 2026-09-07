558cd5f0e21be6a8b7bff9d3a35c4e33338851bf
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:44:40 -0600

    Add logocielo command for cloud logo effect

---
+++ commands/logomaker/logocielo.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logocielo',
+    description: 'Crea un logo con efecto de nubes en el cielo',
+    effectUrl: 'https://en.ephoto360.com/create-a-cloud-text-effect-in-the-sky-618.html'
+});
