ee9f0f8d9730a729b753a90ecb6e88d5b5aa5690
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:40:24 -0600

    Create logosad.ts

---
+++ commands/logomaker/logosad.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logosad',
+    description: 'Crea un logo con texto escrito en vidrio mojado',
+    effectUrl: 'https://en.ephoto360.com/write-text-on-wet-glass-online-589.html'
+});
