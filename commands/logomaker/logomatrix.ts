c2f19410d256042523f7135951c5725fc8025402
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:45:39 -0600

    Add logomatrix command for Matrix text effect

---
+++ commands/logomaker/logomatrix.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logomatrix',
+    description: 'Crea un logo con efecto de código Matrix',
+    effectUrl: 'https://en.ephoto360.com/matrix-text-effect-154.html'
+});
