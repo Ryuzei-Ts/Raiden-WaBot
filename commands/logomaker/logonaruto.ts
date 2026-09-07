2f8126c3b220d80047b6ee91c4c19f2aa707cf7b
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:42:55 -0600

    Add logonaruto command for Naruto logo creation

---
+++ commands/logomaker/logonaruto.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logonaruto',
+    description: 'Crea un logo con estilo de texto Naruto Shippuden',
+    effectUrl: 'https://en.ephoto360.com/naruto-shippuden-logo-style-text-effect-online-808.html'
+});
