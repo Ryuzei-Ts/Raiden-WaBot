9c3e7aec958e89abb13724baad8e4de083a3da52
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:41:46 -0600

    Add logoneon command for neon glitch logo creation

---
+++ commands/logomaker/logoneon.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logoneon',
+    description: 'Crea un logo con efecto de neón glitch',
+    effectUrl: 'https://en.ephoto360.com/create-impressive-neon-glitch-text-effects-online-768.html'
+});
