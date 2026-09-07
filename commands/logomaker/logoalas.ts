dc1954d0a24b70373df5af09431e63d07d74a7f0
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:46:23 -0600

    Add logoalas command for galaxy angel wings effect

---
+++ commands/logomaker/logoalas.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logoalas',
+    description: 'Crea un logo con efecto de alas de ángel galácticas',
+    effectUrl: 'https://en.ephoto360.com/the-effect-of-galaxy-angel-wings-289.html'
+});
