f481099f8e90a84edc4193fa56cb90978f27e2a1
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:47:04 -0600

    Create logopubg.ts

---
+++ commands/logomaker/logopubg.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logopubg',
+    description: 'Crea un logo estilo PUBG con personaje tierno',
+    effectUrl: 'https://en.ephoto360.com/pubg-logo-maker-cute-character-online-617.html'
+});
