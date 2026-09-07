f2db04369e9a1891441a9929c42632f0cf1d98df
Ryuzei <zchpt22zyc@privaterelay.appleid.com>
2026-09-06 19:45:11 -0600

    Add logograffiti3d command for 3D graffiti logo

---
+++ commands/logomaker/logograffiti3d.ts
@@ -0,0 +1,7 @@
+import maker from '#logomaker';
+
+export default maker({
+    command: 'logograffiti3d',
+    description: 'Crea un logo con efecto de graffiti 3D',
+    effectUrl: 'https://en.ephoto360.com/text-graffiti-3d-208.html'
+});
