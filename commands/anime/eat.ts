b1b6aae5e5e27a7f7cd2bfc989ad2c0d2edc6de4
𝐑yuzei 𝐓s ꕀ﹒ <zchpt22zyc@privaterelay.appleid.com>
2026-09-09 03:31:04 -0600

    Add eat command with solo and together phrases

---
+++ commands/anime/eat.ts
@@ -0,0 +1,45 @@
+import { animeMaker } from '#anime';
+
+export default animeMaker({
+    command: ['eat', 'comer'],
+    soloPhrases: {
+        hombre: [
+            'come con gusto',
+            'disfruta de su comida',
+            'saborea cada bocado',
+            'come tranquilamente'
+        ],
+        mujer: [
+            'come con gusto',
+            'disfruta de su comida',
+            'saborea cada bocado',
+            'come tranquilamente'
+        ],
+        otro: [
+            'come con gusto',
+            'disfruta de su comida',
+            'saborea cada bocado',
+            'come tranquilamente'
+        ]
+    },
+    togetherPhrases: {
+        hombre: [
+            'come con',
+            'comparte comida con',
+            'invita a comer a',
+            'disfruta de una comida con'
+        ],
+        mujer: [
+            'come con',
+            'comparte comida con',
+            'invita a comer a',
+            'disfruta de una comida con'
+        ],
+        otro: [
+            'come con',
+            'comparte comida con',
+            'invita a comer a',
+            'disfruta de una comida con'
+        ]
+    }
+});
