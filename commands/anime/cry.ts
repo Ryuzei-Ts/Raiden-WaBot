3b416f4985cf928975b868bb750b9715a3382855
𝐑yuzei 𝐓s ꕀ﹒ <zchpt22zyc@privaterelay.appleid.com>
2026-09-09 03:23:29 -0600

    Add cry command with phrases for different genders

---
+++ commands/anime/cry.ts
@@ -0,0 +1,45 @@
+import { animeMaker } from '#anime';
+
+export default animeMaker({
+    command: ['cry', 'llorar'],
+    soloPhrases: {
+        hombre: [
+            'llora en silencio',
+            'derrama lágrimas de tristeza',
+            'solloza con el corazón roto',
+            'las lágrimas caen sin control'
+        ],
+        mujer: [
+            'llora en silencio',
+            'derrama lágrimas de tristeza',
+            'solloza con el corazón roto',
+            'las lágrimas caen sin control'
+        ],
+        otro: [
+            'llora en silencio',
+            'derrama lágrimas de tristeza',
+            'solloza con el corazón roto',
+            'las lágrimas caen sin control'
+        ]
+    },
+    togetherPhrases: {
+        hombre: [
+            'llora desconsoladamente por',
+            'derrama lágrimas por',
+            'sufre y llora por',
+            'solloza por la ausencia de'
+        ],
+        mujer: [
+            'llora desconsoladamente por',
+            'derrama lágrimas por',
+            'sufre y llora por',
+            'solloza por la ausencia de'
+        ],
+        otro: [
+            'llora desconsoladamente por',
+            'derrama lágrimas por',
+            'sufre y llora por',
+            'solloza por la ausencia de'
+        ]
+    }
+});
