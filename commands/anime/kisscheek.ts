5d8bbf758718203b9ab11b93047a3f78e1224299
𝐑yuzei 𝐓s ꕀ﹒ <zchpt22zyc@privaterelay.appleid.com>
2026-09-09 03:33:01 -0600

    Create kisscheek.ts

---
+++ commands/anime/kisscheek.ts
@@ -0,0 +1,45 @@
+import { animeMaker } from '#anime';
+
+export default animeMaker({
+    command: ['kisscheek', 'beso2'],
+    soloPhrases: {
+        hombre: [
+            'besa su propia mejilla',
+            'se da un beso en la mejilla',
+            'besa el aire con ternura',
+            'sonríe y besa su mejilla'
+        ],
+        mujer: [
+            'besa su propia mejilla',
+            'se da un beso en la mejilla',
+            'besa el aire con ternura',
+            'sonríe y besa su mejilla'
+        ],
+        otro: [
+            'besa su propia mejilla',
+            'se da un beso en la mejilla',
+            'besa el aire con ternura',
+            'sonríe y besa su mejilla'
+        ]
+    },
+    togetherPhrases: {
+        hombre: [
+            'besa la mejilla de',
+            'deja un beso en la mejilla de',
+            'da un beso suave en la mejilla a',
+            'besa tiernamente la mejilla de'
+        ],
+        mujer: [
+            'besa la mejilla de',
+            'deja un beso en la mejilla de',
+            'da un beso suave en la mejilla a',
+            'besa tiernamente la mejilla de'
+        ],
+        otro: [
+            'besa la mejilla de',
+            'deja un beso en la mejilla de',
+            'da un beso suave en la mejilla a',
+            'besa tiernamente la mejilla de'
+        ]
+    }
+});
