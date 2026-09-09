472ebe50c16e2602473fc36b413c03dd1ea6ca1f
𝐑yuzei 𝐓s ꕀ﹒ <zchpt22zyc@privaterelay.appleid.com>
2026-09-09 04:07:14 -0600

    Add lick command for anime interactions

---
+++ commands/anime/lick.ts
@@ -0,0 +1,12 @@
+import { animeMaker } from '#anime';
+
+export default animeMaker({
+    command: ['lick'],
+    description: 'Lame a alguien o a sí mismo',
+    soloPhrases: {
+        global: ['se lame', 'lambe sus labios', 'se lame la mano', 'lame el aire']
+    },
+    togetherPhrases: {
+        global: ['lame a', 'lambe el cuello de', 'le lame la mano a', 'lame suavemente a']
+    }
+});
