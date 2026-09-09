da6706bb86364ee98feb5f13f6ef4d66e61b16e9
𝐑yuzei 𝐓s ꕀ﹒ <zchpt22zyc@privaterelay.appleid.com>
2026-09-09 04:11:44 -0600

    Add gender-specific phrases for 'angry' command

--- commands/anime/angry.ts
+++ commands/anime/angry.ts
@@ -4,9 +4,15 @@ export default animeMaker({
     command: ['angry', 'enojar'],
     description: 'Muestra enojo hacia alguien o a ti mismo',
     soloPhrases: {
-        global: ['está enojado', 'aprieta los puños con rabia', 'su cara refleja ira', 'respira hondo para calmarse']
+        hombre: ['está enojado', 'aprieta los puños con rabia', 'su cara refleja ira', 'respira hondo para calmarse'],
+        mujer: ['está enojada', 'aprieta los puños con rabia', 'su cara refleja ira', 'respira hondo para calmarse'],
+        otro: ['está enojade', 'aprieta los puños con rabia', 'su cara refleja ira', 'respira hondo para calmarse'],
+        indefinido: ['está enojad@', 'aprieta los puños con rabia', 'su cara refleja ira', 'respira hondo para calmarse']
     },
     togetherPhrases: {
-        global: ['se enoja con', 'muestra su ira a', 'está furioso con', 'se enfada por culpa de']
+        hombre: ['se enoja con', 'muestra su ira a', 'está furioso con', 'se enfada por culpa de'],
+        mujer: ['se enoja con', 'muestra su ira a', 'está furiosa con', 'se enfada por culpa de'],
+        otro: ['se enoja con', 'muestra su ira a', 'está furiose con', 'se enfada por culpa de'],
+        indefinido: ['se enoja con', 'muestra su ira a', 'está furioso con', 'se enfada por culpa de']
     }
 });
