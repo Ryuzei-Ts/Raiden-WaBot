4fc252c600d261773063a5ea9d01decfdc000757
𝐑yuzei 𝐓s ꕀ﹒ <zchpt22zyc@privaterelay.appleid.com>
2026-09-09 03:21:40 -0600

    Create kiss.ts

---
+++ commands/anime/kiss.ts
@@ -0,0 +1,45 @@
+import { animeMaker } from '#anime';
+
+export default animeMaker({
+    command: ['kiss', 'beso'],
+    soloPhrases: {
+        hombre: [
+            'disfruta de su propio cariño',
+            'se envuelve en besos',
+            'se da un beso de ánimo',
+            'se besa y sonríe'
+        ],
+        mujer: [
+            'disfruta de su propio cariño',
+            'se envuelve en besos',
+            'se da un beso de ánimo',
+            'se besa y sonríe'
+        ],
+        otro: [
+            'disfruta de su propio cariño',
+            'se envuelve en besos',
+            'se da un beso de ánimo',
+            'se besa y sonríe'
+        ]
+    },
+    togetherPhrases: {
+        hombre: [
+            'dejó un beso en la frente de',
+            'dio un beso a',
+            'lanza un beso a',
+            'besa con pasión a'
+        ],
+        mujer: [
+            'dejó un beso en la frente de',
+            'dio un beso a',
+            'lanza un beso a',
+            'besa con pasión a'
+        ],
+        otro: [
+            'dejó un beso en la frente de',
+            'dio un beso a',
+            'lanza un beso a',
+            'besa con pasión a'
+        ]
+    }
+});
