d77aea9bb39116cefe7b72cfe141f122e5b8c147
𝐑yuzei 𝐓s ꕀ﹒ <zchpt22zyc@privaterelay.appleid.com>
2026-09-09 04:11:04 -0600

    Update bored.ts

--- commands/anime/bored.ts
+++ commands/anime/bored.ts
@@ -4,9 +4,15 @@ export default animeMaker({
     command: ['bored', 'aburrido'],
     description: 'Está aburrido solo o con alguien',
     soloPhrases: {
-        global: ['está aburrido', 'se aburre', 'bosteza de aburrimiento', 'mira al techo aburrido']
+        hombre: ['está aburrido', 'se aburre', 'bosteza de aburrimiento', 'mira al techo aburrido'],
+        mujer: ['está aburrida', 'se aburre', 'bosteza de aburrimiento', 'mira al techo aburrida'],
+        otro: ['está aburride', 'se aburre', 'bosteza de aburrimiento', 'mira al techo aburride'],
+        indefinido: ['está aburrido', 'se aburre', 'bosteza de aburrimiento', 'mira al techo aburrido']
     },
     togetherPhrases: {
-        global: ['se aburre con', 'está aburrido de', 'bosteza por culpa de', 'no soporta el aburrimiento de']
+        hombre: ['se aburre con', 'está aburrido de', 'bosteza por culpa de', 'no soporta el aburrimiento de'],
+        mujer: ['se aburre con', 'está aburrida de', 'bosteza por culpa de', 'no soporta el aburrimiento de'],
+        otro: ['se aburre con', 'está aburride de', 'bosteza por culpa de', 'no soporta el aburrimiento de'],
+        indefinido: ['se aburre con', 'está aburrido de', 'bosteza por culpa de', 'no soporta el aburrimiento de']
     }
 });
