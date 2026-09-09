da1157e7c3d0bc57b1edcc597a052ed8e8440c87
𝐑yuzei 𝐓s ꕀ﹒ <zchpt22zyc@privaterelay.appleid.com>
2026-09-09 03:27:17 -0600

    Add blush command with solo and together phrases

---
+++ commands/anime/blush.ts
@@ -0,0 +1,45 @@
+import { animeMaker } from '#anime';
+
+export default animeMaker({
+    command: ['blush'],
+    soloPhrases: {
+        hombre: [
+            'se sonroja',
+            'sus mejillas se tiñen de rojo',
+            'se ruboriza',
+            'el rubor cubre su rostro'
+        ],
+        mujer: [
+            'se sonroja',
+            'sus mejillas se tiñen de rojo',
+            'se ruboriza',
+            'el rubor cubre su rostro'
+        ],
+        otro: [
+            'se sonroja',
+            'sus mejillas se tiñen de rojo',
+            'se ruboriza',
+            'el rubor cubre su rostro'
+        ]
+    },
+    togetherPhrases: {
+        hombre: [
+            'se sonroja por',
+            'se ruboriza al ver a',
+            'sus mejillas se sonrojan por',
+            'se pone rojo por'
+        ],
+        mujer: [
+            'se sonroja por',
+            'se ruboriza al ver a',
+            'sus mejillas se sonrojan por',
+            'se pone roja por'
+        ],
+        otro: [
+            'se sonroja por',
+            'se ruboriza al ver a',
+            'sus mejillas se sonrojan por',
+            'se pone rojo por'
+        ]
+    }
+});
