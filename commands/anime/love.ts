8051a5bea6e8a6da28109047ba8a717ec69d8728
𝐑yuzei 𝐓s ꕀ﹒ <zchpt22zyc@privaterelay.appleid.com>
2026-09-09 03:24:01 -0600

    Create love.ts

---
+++ commands/anime/love.ts
@@ -0,0 +1,45 @@
+import { animeMaker } from '#anime';
+
+export default animeMaker({
+    command: ['love', 'amor'],
+    soloPhrases: {
+        hombre: [
+            'siente amor en su corazón',
+            'late por amor',
+            'se ama a sí mismo',
+            'su corazón palpita de amor'
+        ],
+        mujer: [
+            'siente amor en su corazón',
+            'late por amor',
+            'se ama a sí misma',
+            'su corazón palpita de amor'
+        ],
+        otro: [
+            'siente amor en su corazón',
+            'late por amor',
+            'se ama a sí mismo',
+            'su corazón palpita de amor'
+        ]
+    },
+    togetherPhrases: {
+        hombre: [
+            'ama con todo su corazón a',
+            'está enamorado de',
+            'siente amor profundo por',
+            'su corazón late por'
+        ],
+        mujer: [
+            'ama con todo su corazón a',
+            'está enamorada de',
+            'siente amor profundo por',
+            'su corazón late por'
+        ],
+        otro: [
+            'ama con todo su corazón a',
+            'está enamorado de',
+            'siente amor profundo por',
+            'su corazón late por'
+        ]
+    }
+});
