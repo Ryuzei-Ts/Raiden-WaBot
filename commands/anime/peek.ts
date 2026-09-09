f7395c4749a6227f5700f7f5bfb327320a699a0f
𝐑yuzei 𝐓s ꕀ﹒ <zchpt22zyc@privaterelay.appleid.com>
2026-09-09 03:32:02 -0600

    Add 'peek' command for anime interactions

---
+++ commands/anime/peek.ts
@@ -0,0 +1,45 @@
+import { animeMaker } from '#anime';
+
+export default animeMaker({
+    command: ['peek'],
+    soloPhrases: {
+        hombre: [
+            'espía detrás de la puerta',
+            'se asoma con curiosidad',
+            'observa con disimulo',
+            'mira desde la esquina'
+        ],
+        mujer: [
+            'espía detrás de la puerta',
+            'se asoma con curiosidad',
+            'observa con disimulo',
+            'mira desde la esquina'
+        ],
+        otro: [
+            'espía detrás de la puerta',
+            'se asoma con curiosidad',
+            'observa con disimulo',
+            'mira desde la esquina'
+        ]
+    },
+    togetherPhrases: {
+        hombre: [
+            'espía a',
+            'observa a escondidas a',
+            'se asoma para ver a',
+            'mira con curiosidad a'
+        ],
+        mujer: [
+            'espía a',
+            'observa a escondidas a',
+            'se asoma para ver a',
+            'mira con curiosidad a'
+        ],
+        otro: [
+            'espía a',
+            'observa a escondidas a',
+            'se asoma para ver a',
+            'mira con curiosidad a'
+        ]
+    }
+});
