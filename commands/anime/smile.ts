5183d15d274950062e5e8298b3d17ac772d1677a
𝐑yuzei 𝐓s ꕀ﹒ <zchpt22zyc@privaterelay.appleid.com>
2026-09-09 03:28:59 -0600

    Create smile.ts

---
+++ commands/anime/smile.ts
@@ -0,0 +1,45 @@
+import { animeMaker } from '#anime';
+
+export default animeMaker({
+    command: ['smile', 'sonreir'],
+    soloPhrases: {
+        hombre: [
+            'sonríe con alegría',
+            'muestra una sonrisa',
+            'sonríe sin razón',
+            'una sonrisa ilumina su rostro'
+        ],
+        mujer: [
+            'sonríe con alegría',
+            'muestra una sonrisa',
+            'sonríe sin razón',
+            'una sonrisa ilumina su rostro'
+        ],
+        otro: [
+            'sonríe con alegría',
+            'muestra una sonrisa',
+            'sonríe sin razón',
+            'una sonrisa ilumina su rostro'
+        ]
+    },
+    togetherPhrases: {
+        hombre: [
+            'sonríe a',
+            'dedica una sonrisa a',
+            'regala una sonrisa a',
+            'sonríe con cariño a'
+        ],
+        mujer: [
+            'sonríe a',
+            'dedica una sonrisa a',
+            'regala una sonrisa a',
+            'sonríe con cariño a'
+        ],
+        otro: [
+            'sonríe a',
+            'dedica una sonrisa a',
+            'regala una sonrisa a',
+            'sonríe con cariño a'
+        ]
+    }
+});
